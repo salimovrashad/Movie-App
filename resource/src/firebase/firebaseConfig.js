ab64acc27420aec56faba6e55f90e0cb7e57e5ce
salimovrashad <selimovresad33@gmail.com>
2023-11-28 00:58:38 +0400

    change firebase

--- resource/src/firebase/firebaseConfig.js
+++ resource/src/firebase/firebaseConfig.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAuajYXxFITeOMVmGa1ZjmV0kwh_uXY86I",
-    authDomain: "movies-da2b8.firebaseapp.com",
-    databaseURL: "https://movies-da2b8-default-rtdb.firebaseio.com",
-    projectId: "movies-da2b8",
-    storageBucket: "movies-da2b8.appspot.com",
-    messagingSenderId: "24429655786",
-    appId: "1:24429655786:web:211cf70e89b0ec91120993"
-};
+    apiKey: "AIzaSyBGMeZYks5e1DdV00M8gVTugpRx_qZ4U6w",
+    authDomain: "movies-c0b19.firebaseapp.com",
+    databaseURL: "https://movies-c0b19-default-rtdb.firebaseio.com",
+    projectId: "movies-c0b19",
+    storageBucket: "movies-c0b19.appspot.com",
+    messagingSenderId: "296426936750",
+    appId: "1:296426936750:web:6bd25d761460989c52903f"
+  };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
